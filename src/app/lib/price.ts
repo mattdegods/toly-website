0323e6e99cfcc562a4d3594f6b1870907fc771f3
mattdegods <matthewcpfeifer@gmail.com>
2023-11-25 22:31:56 -0500

    Improve price function with invalidation + types

--- src/app/lib/price.ts
+++ src/app/lib/price.ts
@@ -1,10 +1,27 @@
-let cache: any = {};
+interface CacheItem {
+  value: number;
+  timestamp: number;
+}
 
-export const getPrice = async (address: string) => {
-  const api_key = process.env.NEXT_PUBLIC_API_KEY!;
-  const cacheDuration = 300000; // Cache duration in milliseconds (e.g., 300000ms = 5 minutes)
+let cache: Record<string, CacheItem> = {};
 
-  // Check if data is in cache and not expired
+// Function to clear cache
+const clearCache = () => {
+  cache = {};
+};
+
+const cacheClearInterval = 3600000; // 1 hour in milliseconds
+setInterval(clearCache, cacheClearInterval);
+
+const cacheDuration = 300000; // Cache duration in milliseconds (5 minutes)
+
+export const getPrice = async (address: string): Promise<number> => {
+  const api_key = process.env.NEXT_PUBLIC_API_KEY;
+
+  if (!api_key) {
+    throw new Error("API key is not set in environment variables.");
+  }
+
   const cachedData = cache[address];
   if (cachedData && Date.now() - cachedData.timestamp < cacheDuration) {
     return cachedData.value;
@@ -18,18 +35,19 @@ export const getPrice = async (address: string) => {
         headers: { "X-API-KEY": api_key },
       }
     );
+
+    if (!response.ok) {
+      throw new Error(`API request failed with status ${response.status}`);
+    }
+
     const data = await response.json();
     const value = data.data.value;
 
-    // Update cache
-    cache[address] = {
-      value: value,
-      timestamp: Date.now(),
-    };
+    cache[address] = { value, timestamp: Date.now() };
 
     return value;
   } catch (error) {
-    console.error(error);
+    console.error("Error fetching price data:", error);
     return 0;
   }
 };
