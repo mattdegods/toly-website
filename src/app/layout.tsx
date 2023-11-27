dbb699d6241a3d1818786c8445d2cee0f6232f16
mattdegods <matthewcpfeifer@gmail.com>
2023-11-26 21:35:31 -0500

    Fix: page meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,20 @@
 import "./globals.css";
 import Head from "next/head";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "$TOLY COIN",
+  description: "FUCK A ROLY, BUY SOME $TOLY",
+  openGraph: {
+    images: ["/images/branding7.png"],
+    description: "FUCK A ROLY, BUY SOME $TOLY",
+  },
+  twitter: {
+    images: ["/images/branding7.png"],
+    card: "summary_large_image",
+    description: "FUCK A ROLY, BUY SOME $TOLY",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -7,33 +22,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
-      <Head>
-        <title>$TOLY COIN</title>
-        <meta name="description" content="FUCK A ROLY, BUY SOME $TOLY" />
-
-        {/* Open Graph / Facebook */}
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="tolycoin.com" />
-        <meta property="og:title" content="$TOLY COIN" />
-        <meta property="og:description" content="FUCK A ROLY, BUY SOME $TOLY" />
-        <meta property="og:image" content="/images/branding7.jpg" />
-
-        {/* Twitter */}
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="tolycoin.com" />
-        <meta property="twitter:title" content="$TOLY COIN" />
-        <meta
-          property="twitter:description"
-          content="FUCK A ROLY, BUY SOME $TOLY"
-        />
-        <meta property="twitter:image" content="/images/branding7.jpg" />
-
-        {/* Additional tags can be added as needed */}
-      </Head>
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    </>
+    <html lang="en">
+      <body>{children}</body>
+    </html>
   );
 }
