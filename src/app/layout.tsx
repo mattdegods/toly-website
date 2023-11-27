c18812b066f9b5d048d837b6eabe2e67d52fb666
mattdegods <matthewcpfeifer@gmail.com>
2023-11-26 21:11:49 -0500

    Add meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,5 @@
-import type { Metadata } from "next";
 import "./globals.css";
-
-export const metadata: Metadata = {
-  title: "$TOLY COIN",
-  description: "FUCK A ROLY, BUY SOME $TOLY",
-};
+import Head from "next/head";
 
 export default function RootLayout({
   children,
@@ -12,8 +7,33 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <>
+      <Head>
+        <title>$TOLY COIN</title>
+        <meta name="description" content="FUCK A ROLY, BUY SOME $TOLY" />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="tolycoin.com" />
+        <meta property="og:title" content="$TOLY COIN" />
+        <meta property="og:description" content="FUCK A ROLY, BUY SOME $TOLY" />
+        <meta property="og:image" content="/images/branding7.jpg" />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="tolycoin.com" />
+        <meta property="twitter:title" content="$TOLY COIN" />
+        <meta
+          property="twitter:description"
+          content="FUCK A ROLY, BUY SOME $TOLY"
+        />
+        <meta property="twitter:image" content="/images/branding7.jpg" />
+
+        {/* Additional tags can be added as needed */}
+      </Head>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </>
   );
 }
